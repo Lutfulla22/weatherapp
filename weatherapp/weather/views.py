e5b5e57fc561ab6ad64c54ffa58ec0aff7965e6a
Lutfulla22 <nlutfulla2112@mail.ru>
2023-01-19 10:43:49 +0500

    remove token

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -5,7 +5,7 @@ from .forms import CityForm
 # Create your views here.
 
 def index(request):
-    appid = '5089e85d1e4314b189fa4b36a54f8e40'
+    appid = 'Token'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
     if(request.method == 'POST'):
